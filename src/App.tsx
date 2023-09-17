bba5428c849385f058b16b56d1cc6ab42207f007
Jonnier <jonnierosjitas@gmail.com>
2023-09-17 18:35:00 -0500

    feat: add Tailwind Themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import MainLayout from './components/layout/MainLayout';
 function App(): JSX.Element {
   return (
     <MainLayout>
-      <div className="flex flex-col gap-10 w-full">
+      <div className=" flex flex-col gap-10 w-full">
         <div className="flex flex-col gap-4">
           <div className="text-5xl font-bold bg-gradient-to-b from-gradient-start via-gradient-middle to-gradient-end bg-clip-text text-transparent">
             Simplus React Boilerplate
